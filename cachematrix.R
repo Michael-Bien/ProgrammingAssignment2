fb73d05184922096aaa71e193b2a5533224e3347
Natalie King <michael.bien@gmail.com>
2020-11-29 20:06:36 -0500

    Completed R Programming Week 3 assignment.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions, combined, will calculate the inverse of a matrix, if it is not already calculated; 
+## however, if the inverted matrix has already been calculated, it returns the cached matrix, saving time
 
-## Write a short comment describing this function
+## makeCacheMatrix returns a list of functions that can set, get, set an inverted matrix, and get an inverted matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  Inverted_Matrix <- NULL     
+  set <- function(y) {  
+    x <<- y
+    Inverted_Matrix <<- NULL
+  }
+  get <- function() x     #get returns x
+  set_Inverted_Matrix <- function(User_Sets_Inverted_Matrix) Inverted_Matrix <<- User_Sets_Inverted_Matrix
+  get_Inverted_Matrix <- function() Inverted_Matrix  
+  list(set = set, get = get,  #listing out the object
+       set_Inverted_Matrix = set_Inverted_Matrix,
+       get_Inverted_Matrix = get_Inverted_Matrix)
+  
 }
 
 
-## Write a short comment describing this function
+#cacheSolve checks to see whether an inverted matrix is stored, and otherwise calculates it 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  
+  Inverted_Matrix <- x$get_Inverted_Matrix()  
+  if(!is.null(Inverted_Matrix)) {  
+    message("getting cached data")
+    return(Inverted_Matrix)
+  }
+  data <- x$get()  
+  Inverted_Matrix <- solve(data, ...)  
+  x$set_Inverted_Matrix(Inverted_Matrix)    
+  Inverted_Matrix    
+  
+  
 }
